8b208da912b1d4d7c8a5f9ef3151c806c06c800c
Marcin Matysek <marcin.matysek@gmail.com>
2024-11-17 22:41:23 +0100

    S02E02

--- tasks/S02E02/S02E02.py
+++ tasks/S02E02/S02E02.py
@@ -9,19 +9,15 @@ class S02E02(BaseTask):
 
     def run(self) -> Dict[str, Any]:
 
-        # Get the path to the 'resource' directory
-        script_dir = os.path.dirname(__file__)  # Directory of the script
-        resource_dir = os.path.join(script_dir, 'resources')
+        images = self.get_images()
 
-        images = []
-        for filename in os.listdir(resource_dir):
-            if not filename.endswith(".png"):  # Skip files that are not .m4a
-                continue
-            file_path = os.path.join(resource_dir, filename)  # Construct full file path
-            image_base64  = encode_image(file_path)
-            images.append({"base64":image_base64})
+        prompt = self.get_prompt()
 
-        #Add in Rules: - Think out loud(in polish) about your task in the "_thinking" field
+        response = OpenAIVService().get_completion(prompt, images, response_format="json_object")
+        self.logger.info(f"response: {response}")
+
+    def get_prompt(self):
+        # Add in Rules: - Think out loud(in polish) about your task in the "_thinking" field
         # and json format {{"_thinking":"", city:"city name"}}
         # if you want to see model reasoning
         prompt = f"""
@@ -34,5 +30,20 @@ class S02E02(BaseTask):
         Response in json format:
         {{city:"city name"}}
         """
-        response = OpenAIVService().get_completion(prompt, images, response_format="json_object")
-        self.logger.info(f"response: {response}")
+        return prompt
+
+    def get_images(self):
+
+        # Get the path to the 'resource' directory
+        script_dir = os.path.dirname(__file__)  # Directory of the script
+        resource_dir = os.path.join(script_dir, 'resources')
+
+        images = []
+        for filename in os.listdir(resource_dir):
+            if not filename.endswith(".png"):  # Skip files that are not .m4a
+                continue
+            file_path = os.path.join(resource_dir, filename)  # Construct full file path
+            image_base64 = encode_image(file_path)
+            images.append({"base64": image_base64})
+
+        return images
