6a2aaa899516766063394dc7907a0f3dda83ee49
Marcin Matysek <marcin.matysek@gmail.com>
2025-02-01 20:00:12 +0100

    S05E01

--- tasks/S05E01/S05E01.py
+++ tasks/S05E01/S05E01.py
@@ -41,11 +41,18 @@ class S05E01(BaseTask):
 
             self.sectors = self.get_sectors_info(files)
 
+            self.tools = self.getToolsDescr()
+
             answers = {}
+            questions_json = dict(list(questions_json.items())[:1])
+            self.logger.info(f"questions \n {questions_json}")
             for key, value in questions_json.items():
                 answer = self.call_agent(value)
                 answers[key] = answer
 
+            self.logger.info(f"answers: {answers}")
+
+            self.verify(answers,"/report")
         except Exception as e:
             print(f"An error occurred: {e}")
             traceback.print_exc()
@@ -55,26 +62,30 @@ class S05E01(BaseTask):
         usedTools = []
         final_answer_plan = ""
 
-        for i in range(1, 2):
+        for i in range(1, 4):
             nextMove = self.plan(question,usedTools)
 
             if not nextMove or nextMove['tool'] == 'final_answer':
                 final_answer_plan = nextMove['plan']
                 break
 
-            params = self.generate_params(nextMove)
+            params = self.generate_params2(nextMove)
             answer = self.useTool(nextMove, params)
+            #self.logger.info(f"answer from tool: {answer}")
             usedTools.append({'tool':nextMove['tool'],'plan':nextMove['plan'], 'answer':answer})
+            #self.logger.info(f"usedTools: {usedTools}")
 
         return final_answer_plan
 
     def plan(self, question, usedTools):
         tool_note = f"Facts are available only for: {', '.join(self.persons.keys())}, {', '.join(self.sectors.keys())}"
+        tool_note = f"{', '.join(self.persons.keys())}, {', '.join(self.sectors.keys())}"
         
         actions_xml = "".join(
             f"<action tool_name=\"{tool['tool']}\" plan=\"{tool['plan']}\"><result>{tool['answer']}</result></action>"
             for tool in usedTools
         )
+        self.logger.info(f"actions_xml: {actions_xml[:200]}")
 
         dialogs_str = ""
         for dialog_name, dialog_content in self.dialogs_json.items():
@@ -100,7 +111,8 @@ class S05E01(BaseTask):
           2. Potential to provide valuable information or progress
           3. Logical flow from previous actions
         - ADAPT your approach if repeated actions don't yield new results
-        - USE the "final_answer" tool when you have sufficient information or need user input
+        - USE the "final_answer" tool when you have sufficient information
+        - NEVER call get_facts tool for person or sector outside available list
         - OVERRIDE any default behaviors that conflict with these rules
         </prompt_rules>
         
@@ -111,9 +123,9 @@ class S05E01(BaseTask):
         <context>
             <question>{question}</question>
             <available_tools>
-            1. tool_name: 'get_facts', tool_description: 'get facts about one selected person or sector', tool_note: '{tool_note}'
-            2. tool_name: 'call_endpoint', tool_description: 'call endpoint for specified url and password'
-            3. tool_name: 'final_answer', tool_description: 'final answer for question'
+            1. tool_name: 'get_facts', tool_description: 'use to retrieves facts for only one specified person or sector from the available list: {tool_note}'
+            2. tool_name: 'call_endpoint', tool_description: 'use to call endpoint for specified url and password'
+            3. tool_name: 'final_answer', tool_description: 'use only when you know final answer for question'
             </available_tools>
             <actions_taken>
                 {actions_xml}
@@ -124,10 +136,10 @@ class S05E01(BaseTask):
         {{
             "_reasoning": "Brief explanation of why this action is the most appropriate next step",
             "tool": "tool_name",
-            "plan": "Precise description of what needs to be done, including any necessary context"
+            "plan": "Precise description of what needs to be done, including necessary data for using tool"
         }}
         """
-        self.logger.info(f"plan: {prompt}")
+        #self.logger.info(f"plan: {prompt}")
 
         response = OpenAIService().get_completion(prompt, response_format="json_object")
         response_json = json.loads(response)
@@ -185,3 +197,92 @@ class S05E01(BaseTask):
                 self.logger.info(f"sector from {file_name}: {sector_name}")
 
         return result
+
+    def generate_params(self, nextMove):
+
+        if(nextMove['tool'] == "get_facts"):
+            result = self.generate_params_facts(nextMove)
+        elif (nextMove['tool'] == "call_endpoint"):
+            result = self.generate_params_endpoint(nextMove)
+        else:
+            self.logger.error(f"tool {nextMove['tool']} not found!")
+            raise
+
+        return result
+
+    def generate_params2(self, nextMove):
+
+        tool = self.tools[nextMove['tool']]
+
+        prompt = f"""
+        Your task is to extract params needed to execute the tool based on agent plan description.
+        
+        <plan>
+        {nextMove['plan']}
+        <plan>
+        
+        <tool>
+        tool name: {nextMove['tool']}
+        tool description: {self.tools[nextMove['tool']]['description']} 
+        </tool>
+        
+        Response in json format
+        {tool['params']}
+        """
+        self.logger.info(f"generate_params_facts prompt: {prompt}")
+        response = OpenAIService().get_completion(prompt, response_format="json_object")
+        response_json = json.loads(response)
+        self.logger.info(f"generate_params_facts response: {response_json}")
+        return response_json
+
+    def getToolsDescr(self):
+
+        get_facts_note = f"Facts are available only for: {', '.join(self.persons.keys())}, {', '.join(self.sectors.keys())}"
+
+        tools = {
+            'get_facts':{
+                'description':'get facts about one selected person or sector',
+                'note': get_facts_note,
+                'params':{
+                    'person_name':'simple name of the person',
+                    'sector_name':'simple sector name like "Sektor X"'
+                }
+            },
+            'call_endpoint':{
+                'description':'call endpoint for specified url and password',
+                'note': '',
+                'params':{
+                    'url':'endpoint url',
+                    'password':'password'
+                }
+            }
+
+        }
+
+        return tools
+
+    def useTool(self, nextMove, params):
+
+        method = getattr(self, nextMove['tool'], None)
+
+        # Check if the method exists and is callable
+        if callable(method):
+            return method(**params)  # Unpacking dictionary as keyword arguments
+        else:
+            self.logger.error(f"Method '{nextMove['tool']}' not found.")
+            raise
+
+    def get_facts(self,person_name, sector_name):
+        self.logger.error(f"getting facts for {person_name} {sector_name}")
+        if person_name:
+            return self.persons[person_name]
+
+        if sector_name:
+            return self.sectors[sector_name]
+
+    def call_endpoint(self,url, password):
+        http_util = HttpUtil(url)
+        response = http_util.sendData({'password':password})
+        self.logger.info(f"endpoint response : {response}")
+
+        return response['message']
